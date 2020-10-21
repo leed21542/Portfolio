67faaacc14dd2e897e4e6420eab8faf056532fae
leed21542 <leed21542@naver.com>
2020-10-22 00:21:59 +0900

    js_navbar_menu_click

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+//넷바 메뉴를 클릭하면 그 메뉴로 이동
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+
+    if(link == null){
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+});
